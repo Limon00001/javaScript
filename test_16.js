1a5a4c9b4538b37e2698d488b984e3d37d11c1bf
Limon00001 <limonhossain82@gmail.com>
2023-10-03 01:07:37 +0600

    10th commit

--- test_16.js
+++ test_16.js
@@ -16,3 +16,41 @@
 // In-line individually:
 export let name = `Andrew`;
 export let age = 45;
+
+
+// All at once at the bottom:
+let item = `water`;
+let price = 25;
+export { item, price };
+
+
+// Export Named function:
+export const message = () => {
+    const name = "Jessica";
+    const age = 40;
+    return `${name} is ${age} years old.`;
+};
+
+
+
+// Default Exports:
+// We can only have one default export in a file.
+
+export let playerName = `Ozil`;
+// export let playerAge = 40;
+let playerAge = 40;
+export default playerAge;
+// At first, need to declare and assign variable. Then [export default variable_name]. Can't use var, let, const after default.
+
+
+// Export Default function:
+    /*
+    const send = () => {
+        const name = "Helen";
+        const age = 70;
+        return `${name} is ${age} years old.`;
+    };
+
+    export default send();
+    // Can't use var, let, const after default.
+*/
--- test_16_external.js
+++ test_16_external.js
@@ -1,5 +1,39 @@
 // --------------------------------------- JavaScript Modules -----------------------------------------------------
 
-// Named Exports:
+// Named Imports:
 import { age, name } from "./test_16.js";
 console.log(name, age);
+
+import { age as Age, name as Name } from "./test_16.js"; // it is called alias
+console.log(Name, Age);
+
+// Import all items:
+import * as all from "./test_16.js";
+console.log(all);
+console.log(all.name);
+
+// Import function:
+import { message } from "./test_16.js";
+console.log(message());
+
+
+
+// Import Default:
+import test from "./test_16.js"; // can use any keyword after import for default 
+console.log(`Default value: ${test}`);
+
+
+// Named & Default Import together:
+// We can only have one default import in a file. Before we declare default import that's why we can't again declare it.
+import { playerName } from "./test_16.js";
+console.log(`Named & Default value: Player Name-> ${playerName} and Player Age-> ${test}`);
+
+// If we didn't declare any default imports the Named & Default Import together it looks like -->
+/* import test, { playerName } from "./test_16.js"; */
+
+
+// Import Function:
+/*
+    import myFunction from "./test_16.js";
+    console.log(myFunction);
+*/
